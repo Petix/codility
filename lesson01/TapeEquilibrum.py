33ad89e00d35b357272931645aa220544159c506
Petix <kosa.peter24@gmail.com>
2014-04-11 00:00:35 +0200

    Update TapeEquilibrum.py

--- lesson01/TapeEquilibrum.py
+++ lesson01/TapeEquilibrum.py
@@ -1,5 +1,23 @@
-codility
-========
+def solution(A):
+    # write your code in Python 2.6 
+    sum=0
+    pivot=[]
+    prev=0
 
-codility projects
-#asfsf
+    l=len(A)-1
+    
+    for k,v in enumerate(A):
+        sum+=v
+        if(k!=l):
+            pivot.insert(k,sum)
+        else:
+            pivot.insert(k,v)
+    min=None
+ 
+    for k,v in enumerate(pivot):
+        firstHalf = v
+        secondHalf = sum - firstHalf
+        diff= abs(firstHalf - secondHalf)
+        if(min==None or min>diff):
+            min=diff
+    return min
